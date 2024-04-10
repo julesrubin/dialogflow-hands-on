51ad8eb411f0e412e8b49cbeb77cf480dcb45b6a
Jules Rubin <jules.rubin@efrei.net>
2024-04-10 15:01:59 +0200

    fix(agent) changed display_name

--- iac/agents.tf
+++ iac/agents.tf
@@ -1,6 +1,6 @@
 resource "google_dialogflow_cx_agent" "agent" {
   project               = var.project_id
-  display_name          = "test-agent"
+  display_name          = "dialogflow-hands-on"
   location              = var.location
   default_language_code = var.default_language_code
   time_zone             = var.time_zone
