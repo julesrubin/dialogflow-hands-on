f24ebd81c07e782593eac15d7a41d192da355b7f
Jules Rubin <jules.rubin@efrei.net>
2024-04-11 23:14:38 +0200

    Update page resource names in pages.tf

--- iac/pages.tf
+++ iac/pages.tf
@@ -1,19 +1,19 @@
-resource "google_dialogflow_cx_page" "artist_overview" {
+resource "google_dialogflow_cx_page" "page_artist_overview" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Artist Overview"
 }
 
-resource "google_dialogflow_cx_page" "product" {
+resource "google_dialogflow_cx_page" "page_product" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Product"
 }
 
-resource "google_dialogflow_cx_page" "product_overview" {
+resource "google_dialogflow_cx_page" "page_product_overview" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Product Overview"
 }
 
-resource "google_dialogflow_cx_page" "music" {
+resource "google_dialogflow_cx_page" "page_music" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Music"
 }
