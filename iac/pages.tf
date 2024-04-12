b82fc98c738017917d56e4f9c9dc5463c35f9bff
Jules Rubin <jules.rubin@efrei.net>
2024-04-12 09:21:43 +0200

    Add entry fulfillment messages to page resources in pages.tf

--- iac/pages.tf
+++ iac/pages.tf
@@ -1,19 +1,48 @@
 resource "google_dialogflow_cx_page" "page_artist_overview" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Artist Overview"
+  entry_fulfillment {
+    messages {
+      text {
+        text = ["Welcome to the Artist Overview page."]
+      }
+    }
+  }
 }
 
 resource "google_dialogflow_cx_page" "page_product" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Product"
+  entry_fulfillment {
+    messages {
+      text {
+        text = ["Welcome to the Product page."]
+      }
+    }
+
+  }
 }
 
 resource "google_dialogflow_cx_page" "page_product_overview" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Product Overview"
+  entry_fulfillment {
+    messages {
+      text {
+        text = ["Welcome to the Product Overview page."]
+      }
+    }
+  }
 }
 
 resource "google_dialogflow_cx_page" "page_music" {
   parent       = google_dialogflow_cx_flow.catalog.id
   display_name = "Music"
+  entry_fulfillment {
+    messages {
+      text {
+        text = ["Welcome to the Music page."]
+      }
+    }
+  }
 }
